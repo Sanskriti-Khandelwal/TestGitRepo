96def09df3a9d4d37e400485d47125c23e1ddbf3
startbit <startbit@DESKTOP-CTBIJ8H>
2023-07-20 17:26:07 +0530

    Add project files.

---
+++ TestGitRepo/Controllers/StudentController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TestGitRepo.Models;
+
+namespace TestGitRepo.Controllers
+{
+    public class StudentController : Controller
+    {
+       
+        private static List<Student> students = new List<Student>
+    {
+        new Student { Id = 1, FirstName = "John", LastName = "Doe" },
+        new Student { Id = 2, FirstName = "Jane", LastName = "Smith" },
+        new Student { Id = 3, FirstName = "Mike ", LastName = "Johnson" }
+       
+    };
+
+     
+        public IActionResult Index()
+        {
+            return View(students);
+        }
+    }
+}
