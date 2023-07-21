39e545f40e9dc229a9e212d603dacfaf1e2f4534
Sanskriti-Khandelwal <133846396+Sanskriti-Khandelwal@users.noreply.github.com>
2023-07-21 17:00:04 +0530

    Update StudentController.cs

--- TestGitRepo/Controllers/StudentController.cs
+++ TestGitRepo/Controllers/StudentController.cs
@@ -9,7 +9,7 @@ namespace TestGitRepo.Controllers
         private static List<Student> students = new List<Student>
     {
         new Student { Id = 1, FirstName = "John", LastName = "Doe" },
-        new Student { Id = 2, FirstName = "Jane", LastName = "Smith" },
+        new Student { Id = 2, FirstName = "Jane", LastName = "Smi" },
         new Student { Id = 3, FirstName = "Mike ", LastName = "Johnson" }
        
     };
